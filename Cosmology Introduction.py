c7e7c76eb15da67056a097e38c2d169511558843
Ryan White <ryanwhite_1@yahoo.com.au>
2022-05-03 17:39:07 +1000

    Cosmo Intro code from lecture

---
+++ Cosmology Introduction.py
@@ -0,0 +1,127 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 16:33:06 2022
+
+@author: ryanw
+"""
+
+# First let's set up our packages
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import integrate
+
+# Write a function for the integrand, i.e. $1/\dot{a}$ (adot inverse)
+# I'm going to keep the constant H_0 outside the integral and add it later.
+def adotinv_flatmatter(a):
+    return np.sqrt(a) 
+
+def adotinv(a,om,ol, orad):
+    adot = a * np.sqrt(orad * a**-4 + om * a**-3 + (1 - om - ol)*a**-2 + ol)
+    return 1.0/adot
+
+# And set some constants
+c = 299792.458 # km/s (speed of light)
+
+H0kmsmpc = 70.  # Hubble constant in km/s/Mpc
+H0s = H0kmsmpc * 3.2408e-20 # H0 in inverse seconds is H0 in km/s/Mpc * (3.2408e-20 Mpc/km)
+H0y = H0s * 3.154e7 * 1.e9 # H0 in inverse Giga years is H0 in inverse seconds * (3.154e7 seconds/year) * (1e9 years / Giga year)
+
+# Let's format that to a more appropriate number of significant figures.  
+# The first % means put the variable here.  The .2f means make it a float with 2 decimal places.
+print('Hubble time = %.2f Gyr'%(1/H0y))
+
+
+
+
+# Now integrate that equation from scalefactor 0 to 1 (as an example) which will give the age of the universe today (because we define a so that a=1 today):
+age_Hubble, uncert = integrate.quad(adotinv_flatmatter,0,1) 
+# This returns age in Hubble times, and the uncertainty in the numerical integral
+
+# Convert that to billions of years
+age_Gyr = age_Hubble/H0y # Age in Gyr
+
+print('Age of the universe in Hubble times = %.3f which is %.2f Gyr'%(age_Hubble,age_Gyr))
+print('Does this match the analytic prediction of 2/3 Hubble times?  Yes!')
+print('')
+print('Uncertainty on the numerical integral is ', uncert, 'so should be negligible.') # It's good to check this to check that the integral has succeeded.
+
+# Note, to get just the age without the uncertainty you can use [0] to get the first element that's returned,
+# and then you can also divide by H0y immediately to get the age in one step
+age_Gyr_easier = integrate.quad(adotinv_flatmatter,0,1)[0] / H0y 
+
+print('Just checking that the one-step age calculation (%.2f Gyr) matches the two step one (%.2f Gyr).'%(age_Gyr_easier,age_Gyr))
+
+# Insert your code here
+age_half_Gyr = integrate.quad(adotinv_flatmatter,0,0.5)[0] / H0y 
+age_double_Gyr = integrate.quad(adotinv_flatmatter,0,2)[0] / H0y 
+
+print("The time at which the universe was half of its current size was", age_half_Gyr, "Gyr")
+print("The time at which the universe will be double its current size is", age_double_Gyr, "Gyr")
+
+
+# Start by making an array of scalefactors
+astart = 0.0
+astop = 2.05
+astep = 0.05 # Make this finer to make the plot smoother
+a_arr = np.arange(astart,astop,astep)
+
+# First set up an array of times (initially all set to zero) into which we'll put our calculated times
+t_Gyr = np.zeros(len(a_arr))  # len(a_arr) gives the length of the a_arr 
+
+# Make a loop and do that integral for every final value of a (starting at a=0 every time)
+for i,a_end in enumerate(a_arr): # enumerate adds an index to each value
+    t_Hubble,uncert = integrate.quad(adotinv_flatmatter,0,a_end)
+    t_Gyr[i] = t_Hubble/H0y
+
+    
+plt.plot(t_Gyr,a_arr)
+plt.plot(age_Gyr, 1.0,'o') # Put a dot at the current time
+#plt.plot(t_analytic_Gyr, a_arr,':',color='red')
+plt.xlabel('Time (Gyr)')
+plt.ylabel('Scalefactor')
+plt.show()
+
+t_lookback_Gyr = np.array([integrate.quad(adotinv_flatmatter, 1, a_end)[0] for a_end in a_arr])/H0y
+
+# Uncomment the commands below to plot your result
+plt.plot(t_lookback_Gyr,a_arr) 
+plt.axvline(x=0,linestyle=':') # Plot some crosshairs 
+plt.axhline(y=1,linestyle=':')
+plt.plot(0.0, 1.0,'o') # Put a dot at the current time
+plt.xlabel('Lookback time (Gyr)')
+plt.ylabel('Scalefactor')
+plt.show()
+
+
+
+# Calculate for the universe we think we live in, with approximately matter density 0.3 and cosmological constant 0.7
+om = 0.3
+ol = 0.7
+orad = 0.01
+
+# Note that when you integrate something with more than one argument you pass it with args=(arg1,arg2) in the integrate function
+# e.g. "integrate.quad(adotinv, lower_limit, uper_limit, args=(om,ol))""
+t_lookback_Gyr = np.array([integrate.quad(adotinv, 1, upper_limit, args=(om,ol, orad))[0] for upper_limit in a_arr])/H0y
+
+plt.plot(t_lookback_Gyr,a_arr,label='$(\Omega_M,\Omega_\Lambda)$=(%.2f,%.2f)'%(om,ol)) 
+plt.axvline(x=0,linestyle=':') # Plot some crosshairs 
+plt.axhline(y=1,linestyle=':')
+plt.xlabel('Lookback time (Gyr)')
+plt.ylabel('Scalefactor')
+plt.legend(loc='lower right',frameon=False)
+plt.show()
+
+om_arr = np.arange(0,2.1,0.4)
+
+for om in om_arr:
+    t_lookback_Gyr = np.array([integrate.quad(adotinv, 1, upper_limit, args=(om, ol, orad))[0] for upper_limit in a_arr])/H0y
+    plt.plot(t_lookback_Gyr,a_arr,label='$(\Omega_M,\Omega_\Lambda)$=(%.1f,%.1f)'%(om,ol))
+
+# Plot this new model (note I've added a label that can be used in the legend)
+plt.plot(t_lookback_Gyr,a_arr,label='$(\Omega_M,\Omega_\Lambda)$=(%.2f,%.2f)'%(om,ol)) 
+plt.axvline(x=0,linestyle=':') # Plot some crosshairs 
+plt.axhline(y=1,linestyle=':')
+plt.xlabel('Lookback time (Gyr)')
+plt.ylabel('Scalefactor')
+plt.legend(loc='lower right',frameon=False)
+plt.show()
