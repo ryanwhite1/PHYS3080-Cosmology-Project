f6194219a9d2ad0e0a70f0b6560cf7341fd4f648
Ryan White <ryanwhite_1@yahoo.com.au>
2022-05-09 13:54:05 +1000

    New Part One Graphs

--- PartOneGraphs.py
+++ PartOneGraphs.py
@@ -14,10 +14,15 @@ def adotinv(a, om, ol, orad):
     adot = a * np.sqrt(orad * a**-4 + om * a**-3 + ok * a**-2 + ol)
     return 1.0/adot
 
+def aadotinv(a, om, ol, orad):
+    ok = 1 - om - ol - orad
+    adot = a**2 * np.sqrt(orad * a**-4 + om * a**-3 + ok * a**-2 + ol)
+    return 1.0/adot
+
 def Ez(z, om, ol, orad):
     ok = 1 - om - ol - orad
     Ez = np.sqrt(orad * (1 + z)**4 + om * (1 + z)**3 + ok * (1 + z)**2 + ol) # Put your code here!  This is not right until you change it.
-    return Ez
+    return 1 / Ez
 
 def Sk(xx, om, ol, orad):
     ok = 1 - om - ol - orad
@@ -67,8 +72,8 @@ ax.plot(t_lookback_Gyr,a_arr,label='$(\Omega_M,\Omega_\Lambda)$=(%.2f,%.2f)'%(om
 ax.axvline(x=0,linestyle=':'); plt.axhline(y=1,linestyle=':') # Plot some crosshairs 
 ax.set_xlabel('Lookback time (Gyr)'); ax.set_ylabel('Scalefactor')
 ax.legend(loc='lower right',frameon=False)
-fig.savefig(dir_path+f'\\Part One Graphs\\Scalefactor-vs-LookbackTime for Our Universe.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
-fig.savefig(dir_path+f'\\Part One Graphs\\Scalefactor-vs-LookbackTime for Our Universe.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Scalefactor-vs-LookbackTime for Our Universe.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Scalefactor-vs-LookbackTime for Our Universe.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
 plt.close(fig)
 
 
@@ -83,8 +88,17 @@ for om in om_arr:
 ax.axvline(x=0,linestyle=':'); ax.axhline(y=1,linestyle=':') #Plot some crosshairs 
 ax.set_xlabel('Lookback time (Gyr)'); ax.set_ylabel('Scalefactor ($a$)')  
 ax.legend(loc='upper left',frameon=False)
-fig.savefig(dir_path+f'\\Part One Graphs\\Scalefactor-vs-LookbackTime.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
-fig.savefig(dir_path+f'\\Part One Graphs\\Scalefactor-vs-LookbackTime.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Scalefactor-vs-LookbackTime.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Scalefactor-vs-LookbackTime.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+plt.close(fig)
+
+
+fig, ax = plt.subplots()
+redshift = -1 + (1 / a_arr)
+ax.plot(a_arr, redshift)
+ax.set_xlabel("Scalefactor ($a$)"); ax.set_ylabel("Redshift ($z$)")
+fig.savefig(dir_path+'\\Part One Graphs\\Redshift vs Scalefactor.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Redshift vs Scalefactor.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
 plt.close(fig)
 
 
@@ -110,16 +124,16 @@ fig, ax = plt.subplots()
 ax.plot(zarr,R0X)
 ax.set_xlabel('Redshift ($z$)'); ax.set_ylabel('$R_0\chi$ (Glyr)')
 ax.set_xlim(xmin=0); ax.set_ylim(ymin=0)
-fig.savefig(dir_path+f'\\Part One Graphs\\Comoving Distance vs Redshift.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
-fig.savefig(dir_path+f'\\Part One Graphs\\Comoving Distance vs Redshift.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Comoving Distance vs Redshift.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Comoving Distance vs Redshift.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
 plt.close(fig)
 
 fig, ax = plt.subplots()
 ax.plot(zarr,aR0X)
 ax.set_xlabel('Redshift ($z$)'); ax.set_ylabel('$aR_0\chi$ (Glyr)')
 ax.set_xlim(xmin=0); ax.set_ylim(ymin=0)
-fig.savefig(dir_path+f'\\Part One Graphs\\Emission Distance vs Redshift.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
-fig.savefig(dir_path+f'\\Part One Graphs\\Emission Distance vs Redshift.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Emission Distance vs Redshift.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Emission Distance vs Redshift.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
 plt.close(fig)
 
 
@@ -135,8 +149,34 @@ plt.plot(zarr, DA, label='Angular Diameter Distance')
 ax.legend()
 ax.set_xlabel('Redshift ($z$)'); ax.set_ylabel('Distances (Glyr)')
 ax.set_xlim(xmin=0)
-fig.savefig(dir_path+f'\\Part One Graphs\\DL and DA vs Redshift.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
-fig.savefig(dir_path+f'\\Part One Graphs\\DL and DA vs Redshift.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\DL and DA vs Redshift.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\DL and DA vs Redshift.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+plt.close(fig)
+
+
+ScaleArr = np.arange(0.001, 2, 0.001)
+ParticleHoriz = np.zeros(len(ScaleArr))
+EventHoriz = np.zeros(len(ScaleArr))
+HubbleHoriz = np.zeros(len(ScaleArr))
+for i, a in enumerate(ScaleArr):
+    ParticleHoriz[i] = integrate.quad(aadotinv, 0, a, args=(om, ol, orad))[0]
+    EventHoriz[i] = integrate.quad(aadotinv, a, np.inf, args=(om, ol, orad))[0]
+    HubbleHoriz[i] = adotinv(a, om, ol, orad)
+    
+# Sub in the required constants to get the comoving distance R_0*X
+ParticleHoriz = ParticleHoriz * cH0Glyr # Distance in Glyr
+EventHoriz = EventHoriz * cH0Glyr
+HubbleHoriz = HubbleHoriz * cH0Glyr
+
+fig, ax = plt.subplots()
+ax.plot(ParticleHoriz, ScaleArr, color='g', label="Particle Horizon"); ax.plot(-ParticleHoriz, ScaleArr, color='g')
+ax.plot(EventHoriz, ScaleArr, color='b', label="Event Horizon"); ax.plot(-EventHoriz, ScaleArr, color='b')
+ax.plot(HubbleHoriz, ScaleArr, color='r', label="Hubble Sphere"); ax.plot(-HubbleHoriz, ScaleArr, color='r')
+ax.legend(loc="lower right"); ax.grid(which="major")
+ax.set_xlabel('Comoving Distance $R_0 \chi$ (Glyr)'); ax.set_ylabel('Scalefactor ($a$)')
+#ax.set_xlim(xmin=0); ax.set_ylim(ymin=0)
+fig.savefig(dir_path+'\\Part One Graphs\\Spacetime Diagram.png', dpi=200, bbox_inches='tight', pad_inches = 0.01)
+fig.savefig(dir_path+'\\Part One Graphs\\Spacetime Diagram.pdf', dpi=200, bbox_inches='tight', pad_inches = 0.01)
 plt.close(fig)
 
 
